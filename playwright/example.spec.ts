e797038b55dffebbf05bbc3e364aefb344386436
Felix Mokross <felix.mokross@zuehlke.com>
2023-03-31 23:48:23 +0200

    Complete signup test

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -15,14 +15,26 @@ test("sign up new user", async ({ page }) => {
 
   await expect(page).toHaveTitle(/Sign up | Cashfolio/);
 
-  await page.getByLabel("Email address").fill(`test@example.com`);
+  await page.getByLabel("Email address").fill(`new_user@example.com`);
   await page.getByLabel("Password").fill("testPassword_1234");
 
   await page.getByRole("button", { name: "Continue" }).click();
+
+  // Although we are a first-party application for the IdP
+  // user consent is required by Auth0 since we are running on localhost
   await page.getByRole("button", { name: "Accept" }).click();
 
   await expect(
     page.getByRole("heading", { name: "Complete Signup" })
+  ).toBeVisible();
+
+  await page.getByRole("combobox", { name: "Main Currency" }).type("CHF");
+  await page.getByRole("combobox", { name: "Main Currency" }).press("Enter");
+
+  await page.getByRole("button", { name: "Start Using Cashfolio" }).click();
+
+  await expect(
+    page.getByRole("heading", { name: "Hello world!" })
   ).toBeVisible();
 });
 
