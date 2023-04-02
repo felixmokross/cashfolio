051624319f382fadc1923c4e6f40851deaad57ac
Felix Mokross <felix.mokross@zuehlke.com>
2023-04-02 22:23:34 +0200

    Enable remaining Remix v2 feature flags

--- remix.config.js
+++ remix.config.js
@@ -8,6 +8,8 @@ module.exports = {
   future: {
     v2_routeConvention: true,
     v2_meta: true,
+    v2_errorBoundary: true,
+    v2_normalizeFormMethod: true,
     unstable_tailwind: true,
   },
 };
