5fc626e74bf9b7260415b3326886dbc6d818f451
Felix Mokross <felix.mokross@zuehlke.com>
2023-04-02 21:37:03 +0200

    Move user data and logout link to header

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -1,11 +1,22 @@
-import { Link, Outlet } from "@remix-run/react";
+import type { DataFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { getUser } from "~/auth.server";
+
+export async function loader({ request }: DataFunctionArgs) {
+  const user = await getUser(request);
+  return json({ user });
+}
 
 export default function App() {
+  const { user } = useLoaderData<typeof loader>();
   return (
     <div>
       <p>
         <Link to=".">Home</Link> | <Link to="accounts">Accounts</Link> |{" "}
-        <Link to="asset-classes">Asset Classes</Link>
+        <Link to="asset-classes">Asset Classes</Link> | User: {user.email}
+        <img className="h-10 w-10" src={user.pictureUrl} alt="" /> |{" "}
+        <Link to="logout">Log Out</Link>
       </p>
       <Outlet />
     </div>
--- app/routes/__app/index.tsx
+++ app/routes/__app/index.tsx
@@ -1,26 +1,7 @@
-import type { DataFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { getUser } from "~/auth.server";
-
-export async function loader({ request }: DataFunctionArgs) {
-  const user = await getUser(request);
-  return json({ user });
-}
-
 export default function Index() {
-  const { user } = useLoaderData<typeof loader>();
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p>
-        User: {user.email}
-        <img className="h-10 w-10" src={user.pictureUrl} alt="" />
-      </p>
-
-      <p>
-        <Link to="logout">&rarr; Log Out</Link>
-      </p>
     </>
   );
 }
