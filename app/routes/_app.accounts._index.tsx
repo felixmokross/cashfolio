630a99370fbbf02d63d3da163c7ce6aa44ba2aa0
Felix Mokross <felix.mokross@zuehlke.com>
2023-04-30 23:00:59 +0200

    WIP new account page

--- app/routes/_app.accounts._index.tsx
+++ app/routes/_app.accounts._index.tsx
@@ -1,4 +1,11 @@
-import type { DataFunctionArgs, V2_MetaFunction } from "@remix-run/node";
+import { PlusIcon, PresentationChartLineIcon } from "@heroicons/react/20/solid";
+import { BanknotesIcon, BuildingOffice2Icon } from "@heroicons/react/20/solid";
+import type { Account } from "@prisma/client";
+import type {
+  DataFunctionArgs,
+  SerializeFrom,
+  V2_MetaFunction,
+} from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { requireUserId } from "~/auth.server";
@@ -18,21 +25,22 @@ export const meta: V2_MetaFunction = () => [{ title: getTitle("Accounts") }];
 export default function AccountListPage() {
   const { accounts } = useLoaderData<typeof loader>();
   return (
-    <div className="h-screen bg-slate-50">
-      <h2 className="mt-4 text-center text-lg font-medium text-slate-700">
-        Accounts
-      </h2>
+    <div className="px-4">
+      <div className="mt-4 flex items-baseline justify-between ">
+        <h2 className="text-lg font-medium text-slate-800">Accounts</h2>
 
-      <Button as={Link} to="new">
-        New Account
-      </Button>
+        {/* TODO Introduce Icon button */}
+        <Button as={Link} to="new" className="inline-flex items-center gap-1.5">
+          <PlusIcon className="-ml-1.5 h-4 w-4" />
+          New
+        </Button>
+      </div>
 
       <ul className="mt-4 space-y-4">
         {accounts.map((a) => (
-          <Account
+          <AccountListItem
             key={a.id}
-            slug={a.slug}
-            name={a.name}
+            account={a}
             balance="EUR 10'000"
             balanceInRefCurrency="CHF 11'000"
           />
@@ -42,18 +50,35 @@ export default function AccountListPage() {
   );
 }
 
-type AccountProps = {
-  slug: string;
-  name: string;
+type AccountListItemProps = {
+  account: Pick<SerializeFrom<Account>, "name" | "slug" | "unit">;
   balance: string;
-  balanceInRefCurrency?: string;
+  balanceInRefCurrency: string | undefined;
 };
 
-function Account({ slug, name, balance, balanceInRefCurrency }: AccountProps) {
+function AccountListItem({
+  account,
+  balance,
+  balanceInRefCurrency,
+}: AccountListItemProps) {
   return (
-    <li className="mx-2 flex h-20 items-stretch justify-between rounded-md border border-slate-300 bg-white px-4 py-2">
-      <Link to={slug}>
-        <h3 className="text-sm font-medium text-slate-700">{name}</h3>
+    <li className="">
+      <Link
+        to={account.slug}
+        className="group inline-flex w-full items-center justify-between"
+      >
+        <div className="flex items-center gap-4">
+          <div className="text-slate-400 group-hover:text-slate-500">
+            {account.unit === "CURRENCY" ? (
+              <BanknotesIcon className="h-8 w-8" />
+            ) : account.unit === "STOCK" ? (
+              <PresentationChartLineIcon className="h-8 w-8" />
+            ) : null}
+          </div>
+          <h3 className="text-sm font-semibold text-slate-700 group-hover:text-slate-800">
+            {account.name}
+          </h3>
+        </div>
         <div className="text-right">
           <div className="text-lg text-slate-700">{balance}</div>
           {balanceInRefCurrency && (
