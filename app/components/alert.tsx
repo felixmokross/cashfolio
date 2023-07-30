a12e97d8ac9c780c49385701234542d4f220b1e7
Felix Mokross <felix.mokross@zuehlke.com>
2023-07-30 12:20:11 +0200

    Implement dismiss in Alert component

--- app/components/alert.tsx
+++ app/components/alert.tsx
@@ -1,10 +1,12 @@
 import { CheckCircleIcon, XMarkIcon } from "@heroicons/react/20/solid";
-import type { PropsWithChildren } from "react";
+import { useState, type PropsWithChildren } from "react";
 import { cn } from "./classnames";
 
 type AlertProps = PropsWithChildren<{ className?: string }>;
 
 export function Alert({ children, className }: AlertProps) {
+  const [isDismissed, setIsDismissed] = useState(false);
+  if (isDismissed) return null;
   return (
     <div className={cn("rounded-md bg-emerald-50 p-4", className)}>
       <div className="flex">
@@ -22,6 +24,7 @@ export function Alert({ children, className }: AlertProps) {
             <button
               type="button"
               className="inline-flex rounded-md bg-emerald-50 p-1.5 text-emerald-500 hover:bg-emerald-100 focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:ring-offset-2 focus:ring-offset-emerald-50"
+              onClick={() => setIsDismissed(true)}
             >
               <span className="sr-only">Dismiss</span>
               <XMarkIcon className="h-5 w-5" aria-hidden="true" />
