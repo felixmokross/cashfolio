babfc2ad38c601a1915c18b3181ab1dd0a3ed8da
Felix Mokross <felix.mokross@zuehlke.com>
2023-07-30 11:49:58 +0200

    Opt into new Remix features

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,4 @@
-/** @type {import('@remix-run/dev').AppConfig} */
+/** @type {import("@remix-run/dev").AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   serverModuleFormat: "cjs",
@@ -8,5 +8,7 @@ module.exports = {
     v2_meta: true,
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
+    v2_headers: true,
+    v2_dev: true,
   },
 };
