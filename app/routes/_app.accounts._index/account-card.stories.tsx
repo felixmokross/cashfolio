80b8d528827f981cb801c587f92f405d31970072
Felix Mokross <felix.mokross@zuehlke.com>
2023-08-04 23:18:33 +0200

    Improve AccountCard stories

--- app/routes/_app.accounts._index/account-card.stories.tsx
+++ app/routes/_app.accounts._index/account-card.stories.tsx
@@ -1,20 +1,21 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { AccountCard } from "./account-card";
 import { AccountUnit } from "@prisma/client";
+import { withAppProviders } from "../../../.storybook/decorators/withAppProviders";
+import { buildAccountDto } from "~/accounts/builders";
 
 const meta: Meta<typeof AccountCard> = {
-  title: "routes/accounts/_index/AccountCard",
+  title: "routes/_app/accounts/_index/AccountCard",
   component: AccountCard,
-  tags: ["autodocs"],
   decorators: [
     (Story) => (
-      <div className="flex items-center justify-center">
-        <div className="w-full max-w-xs">
-          <Story />
-        </div>
+      <div className="w-72">
+        <Story />
       </div>
     ),
+    withAppProviders,
   ],
+  parameters: { layout: "centered" },
 };
 
 export default meta;
@@ -23,12 +24,7 @@ type Story = StoryObj<typeof AccountCard>;
 
 export const Default: Story = {
   args: {
-    account: {
-      name: "Cash",
-      slug: "cash",
-      unit: AccountUnit.CURRENCY,
-      currency: "CHF",
-    },
+    account: buildAccountDto({ name: "Cash", slug: "cash" }),
     balance: "10000",
     balanceInRefCurrency: "10000",
   },
@@ -36,12 +32,11 @@ export const Default: Story = {
 
 export const ForeignCurrency: Story = {
   args: {
-    account: {
+    account: buildAccountDto({
       name: "Foreign Cash",
       slug: "foreign-cash",
-      unit: AccountUnit.CURRENCY,
       currency: "EUR",
-    },
+    }),
     balance: "10000",
     balanceInRefCurrency: "11000",
   },
@@ -49,12 +44,12 @@ export const ForeignCurrency: Story = {
 
 export const Stock: Story = {
   args: {
-    account: {
+    account: buildAccountDto({
       name: "Stock",
       slug: "stock",
       unit: AccountUnit.STOCK,
       currency: null,
-    },
+    }),
     balance: "1240",
     balanceInRefCurrency: "14000",
   },
@@ -62,12 +57,12 @@ export const Stock: Story = {
 
 export const LongContent: Story = {
   args: {
-    account: {
+    account: buildAccountDto({
       name: "This is a Long Account Name",
       slug: "long-account",
       unit: AccountUnit.CURRENCY,
       currency: "USD",
-    },
+    }),
     balance: "10000",
     balanceInRefCurrency: "11000",
   },
