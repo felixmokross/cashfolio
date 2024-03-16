157f4e5b44acc600cc36e80709c19b42a43a9998
Felix Mokross <felix.mokross@zuehlke.com>
2024-03-16 14:03:29 +0100

    Switch to react-docgen-typescript

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,5 +15,10 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  typescript: {
+    // Experiencing issues with the default 'react-docgen' since Storybook 8 for some components, therefore switching to 'react-docgen-typescript'
+    // See https://storybook.js.org/docs/api/main-config-typescript#reactdocgen
+    reactDocgen: "react-docgen-typescript",
+  },
 };
 export default config;
