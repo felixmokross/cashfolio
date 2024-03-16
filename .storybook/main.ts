3be03050f5ab3a35844cf1a73b36dad403c3165f
Felix Mokross <felixmokross@gmail.com>
2024-03-16 14:17:53 +0100

    updates (#31)
    * npm update
    
    * Upgrade Storybook
    
    * Upgrade chromatic
    
    * Storybook: Remove onboarding addon
    
    * Fix playwright version (chromium has an issue in 1.42)
    
    * Add webkit, install only required browsers
    
    * Switch to react-docgen-typescript
    
    * Remove webkit again – makes it too unreliable

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,9 +5,8 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling",
+    "@chromatic-com/storybook",
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -15,6 +14,11 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  typescript: {
+    // Experiencing issues with the default 'react-docgen' since Storybook 8 for some components, therefore switching to 'react-docgen-typescript'
+    // See https://storybook.js.org/docs/api/main-config-typescript#reactdocgen
+    reactDocgen: "react-docgen-typescript",
   },
 };
 export default config;
